bffb15d37ad2f5509ebd52c614d460f9ecd961cd
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-02-18 23:36:56 +0100

    Update facebook.js

--- handler/facebook.js
+++ handler/facebook.js
@@ -17,11 +17,11 @@ const message = (senderPsid, receivedMessage) => {
             'title': 'Hello Msg 👋',
             'subtitle': 'first login to your account',
             'buttons': [
-              {
-                'type': 'account_link',
-                'title': 'Login',
-                'url': 'https://quickbot.onrender.com/account/link',
-              },
+              // {
+              //   'type': 'account_link',
+              //   'title': 'Login',
+              //   'url': 'https://quickbot.onrender.com/account/link',
+              // },
               {
                 'type': 'postback',
                 'title': 'stay anonimos',
