2bf96d0cbc8e011c630cfd3ee7ea11511612a43c
Laziri <109730158+lazirijs@users.noreply.github.com>
2024-02-19 22:49:27 +0100

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -14,12 +14,7 @@ app.get('/login', (req, res) => {
 }););
 
 app.get('/logout', (req, res) => {
-  console.log('link request query : ', req.query);
-
-  // Implement logic to verify user identity and link accounts
-  // ...
-  console.log(`Account Logged out`);
-  // Respond to Facebook with the result of the Account Linking process
+  console.log(`Account Logged out \n`, 'request query : ', req.query);
   res.status(200).send(`Account Logged out`);
 });
 
